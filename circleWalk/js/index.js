0b71fe12794a13e1e7d623f2d81f352a89031949
VIRGIL-P <102518459+VIRGIL-P@users.noreply.github.com>
2022-06-07 14:22:07 +0300

    Add files via upload

--- circleWalk/js/index.js
+++ circleWalk/js/index.js
@@ -1,35 +1,25 @@
-//Помещаем элементы управления в переменные
-let leftButton = document.getElementsByClassName('leftButton')[0];
-var rightButton = document.getElementById('rightButton');
-const topButton = document.getElementsByTagName('p')[0];
-const bottomButton = document.querySelector('.bottomButton');
+let leftButton = document.querySelector('.leftButton');
+let rightButton = document.querySelector('.rightButton');
+let topButton = document.querySelector('.topButton');
+let bottomButton = document.querySelector('.bottomButton')
 
-//Помещаем наш круг в переменную
 const square = document.querySelector('.circle');
 
-//Вешаем прослушку на кнопку "влево" через ранее созданную переменную, используя событие типа "click"
-//Функцию описываем внутри прослушки
 leftButton.addEventListener("click", function() {
     square.classList.remove('right');
 })
 
-//Создаём функцию для правой кнопки вне прослушки
-function rigthAction() {
-    square.classList.add('right');
-}
-
-//Вешаем прослушку на кнопку "вправо" используя событие типа "mousedown"
-//(событие активируется как только пользователь нажмёт на кнопу и не успеет её отпустить)
-//Функция для прослушки описана вне самой прослушки
-rightButton.addEventListener('mousedown', rigthAction);
-
-topButton.addEventListener('click', function() {
-//Внутри функции используется объект classList и его метод remove
+topButton.addEventListener("click", function() {
     square.classList.remove('bottom');
 })
 
-bottomButton.addEventListener('click', function() {
-    square.classList.add('botom');
-})
+function rightAction() {
+    square.classList.add('right');
+}
+rightButton.addEventListener('mousedown', rightAction);
 
-//По возможности сделай код в едином стиле и верни работоспособность всем кнопкам
+function bottomAction() {
+    square.classList.add('bottom');
+}
+bottomButton.addEventListener('mousedown', bottomAction);
+
